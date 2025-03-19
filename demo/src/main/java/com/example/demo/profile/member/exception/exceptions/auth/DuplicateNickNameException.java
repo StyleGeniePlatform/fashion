47a71f7be2cd55ce617762ddbd1084a9e96a6567
승엽 <tmdduqflfl@naver.com>
2025-03-19 19:18:43 +0900

    feat: exception

---
+++ demo/src/main/java/com/example/demo/profile/member/exception/exceptions/auth/DuplicateNickNameException.java
@@ -0,0 +1,11 @@
+package com.example.demo.profile.member.exception.exceptions.auth;
+
+import com.example.demo.profile.member.exception.exceptions.MemberErrorCode;
+import com.example.demo.profile.member.exception.exceptions.MemberException;
+
+public class DuplicateNickNameException extends MemberException {
+
+    public DuplicateNickNameException() {
+        super(MemberErrorCode.DUPLICATED_NICK_NAME);
+    }
+}
